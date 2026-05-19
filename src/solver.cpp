19673c3901efa30ef8083d7f762c14d7944bdf1e
K <Akarfire@gmail.com>
2026-05-19 20:18:54 +0300

    [DEV] Fixed function types and arguments

--- src/solver.cpp
+++ src/solver.cpp
@@ -1,1 +1,11 @@
 #include "solver.h"
+
+std::vector<double> Solver::solveSeidelMethod(FFuncType f,
+                                              double a, double b, 
+                                              double c, double d,
+                                              BoundaryFuncType mu1, BoundaryFuncType mu2, 
+                                              BoundaryFuncType mu3, BoundaryFuncType mu4,
+                                              unsigned n, unsigned m)
+{
+    return {};
+}
