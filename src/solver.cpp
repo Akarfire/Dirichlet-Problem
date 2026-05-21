277babf7ec2bab1a33054dcd1a6afeb579c9bdc3
K <Akarfire@gmail.com>
2026-05-21 18:10:31 +0300

    [SYNC] Resolving conflicts with H1nk

--- src/solver.cpp
+++ src/solver.cpp
@@ -1,14 +1,14 @@
 #include "solver.h"
 
-inline double Solver::Solution::getX(unsigned ind) {
+inline double Solver::SeidelSolution::getX(unsigned ind) {
     return a + ind * x_step;
 }
 
-inline double Solver::Solution::getY(unsigned ind) {
+inline double Solver::SeidelSolution::getY(unsigned ind) {
     return b + ind * y_step;
 }
 
-std::vector<std::vector<double>> Solver::Solution::solve() {
+std::vector<std::vector<double>> Solver::SeidelSolution::solve() {
     x_step = (b - a) / n;
     y_step = (d - c) / m;
 
@@ -25,11 +25,13 @@ std::vector<std::vector<double>> Solver::Solution::solve() {
         V[i][m] = mu4(getX(i));
     }
 
-    for (unsigned j = 1; j < m - 1; j++) {
+    for (unsigned j = 1; j < m + 1; j++) {
         V[0][j] = mu1(getY(j));
         V[n][j] = mu2(getY(j));
     }
 
+    // Seidel core
+    // TODO пока без выхода по эпсилон, надо будет добавить
     for (unsigned iteration = 1; iteration <= ITERMAX; iteration++) {
         for (unsigned j = 1; j < m; j++) {
             for (unsigned i = 1; i < n; i++) {
@@ -54,6 +56,6 @@ std::vector<std::vector<double>> Solver::solveSeidelMethod(FFuncType f,
                                               unsigned n, unsigned m,
                                               unsigned NMAX)
 {
-    Solution solution(f, a, b, c, d, mu1, mu2, mu3, mu4, n, m, NMAX);
-    return solution.solve();
+    SeidelSolution SeidelSolution(f, a, b, c, d, mu1, mu2, mu3, mu4, n, m, NMAX);
+    return SeidelSolution.solve();
 }
